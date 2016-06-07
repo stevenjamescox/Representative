50c7557231310de4b6e25a0faa931520794d4875
Steve Cox <coxsteve@gmail.com>
2016-06-07 15:22:12 -0600

    first half of network controller built

--- NetworkController.swift
+++ NetworkController.swift
@@ -12,20 +12,30 @@ class NetworkController {
 
     static func dataAtURL(url: String, completion: (data: NSData?) -> Void) {
     guard let url = NSURL(string: url) else { completion(data: nil); return }
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, _, error) in
+            
+            if let error = error {
+            print(error.localizedDescription)
+            completion(data: nil)
+                return
+            }
+            
+            if let data = data {
+            print("data retrieved")
+            completion(data: data)
+                return
+            }
+            
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        task.resume()
     }
+    
+    
+    
+    
+    
+    
+    
 }
