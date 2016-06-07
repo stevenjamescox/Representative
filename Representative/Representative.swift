5c4aadfa348e90c126ac27f4a091a59acfa4d804
Steve Cox <coxsteve@gmail.com>
2016-06-07 15:10:11 -0600

    edited .plist, getting started on Model Object

---
+++ Representative/Representative.swift
@@ -0,0 +1,29 @@
+//
+//  Representative.swift
+//  Representative
+//
+//  Created by Steve Cox on 6/7/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Representative {
+    
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+    
+
+    
+
+
+// baseURL http://whoismyrepresentative.com/getall_reps_bystate.php?state=CA
+
+
+
+}
