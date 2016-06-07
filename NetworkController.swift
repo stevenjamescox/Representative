ffedcd29d60051313f06eed7859a4798f49f6fb4
Steve Cox <coxsteve@gmail.com>
2016-06-07 15:16:03 -0600

    starting NetworkController build

---
+++ NetworkController.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkController.swift
+//  Representative
+//
+//  Created by Steve Cox on 6/7/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+
+    static func dataAtURL(url: String, completion: (data: NSData?) -> Void) {
+    guard let url = NSURL(string: url) else { completion(data: nil); return }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
